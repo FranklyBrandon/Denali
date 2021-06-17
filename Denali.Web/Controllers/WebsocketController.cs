8241e83d8bdff7797853d41f9e89fc3ee4221f76
FranklyBrandon <bran.frankenfield@gmail.com>
2021-06-16 21:45:11 -0500

    add websocket to controller

---
+++ Denali.Web/Controllers/WebsocketController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Denali.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebsocketController : ControllerBase
+    {
+        [HttpGet]
+        public async Task GetConnect()
+        {
+            if (this.HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await this.HttpContext.WebSockets.AcceptWebSocketAsync();
+                while (!HttpContext.RequestAborted.IsCancellationRequested)
+                {
+                    var response = string.Format("Hello! Time {0}", System.DateTime.Now.ToString());
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(response);
+
+                    await webSocket.SendAsync(new System.ArraySegment<byte>(bytes),
+                        WebSocketMessageType.Text, true, CancellationToken.None);
+
+                    await Task.Delay(2000);
+                }       
+            }
+        }
+    }
+}
