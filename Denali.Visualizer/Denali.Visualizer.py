e0c2fffe449fc1730424a370ab6d251eba385842
FranklyBrandon <bran.frankenfield@gmail.com>
2021-11-03 16:15:26 -0500

    add alpaca data loader

--- Denali.Visualizer/Denali.Visualizer.py
+++ Denali.Visualizer/Denali.Visualizer.py
@@ -1,26 +1,19 @@
 from re import X
 import plotly.graph_objects as go
 import json, os
+from MovingAverages import get_smas, get_emas
+from AlpacaDataLoader import get_alpaca_candle_data, AggregateData
 
 def main():
+    aggregateData = get_alpaca_candle_data()
 
-    dir = "C:\\Denali\\Denali.Visualizer"
-    fileName = "11_1_2021_AAPL.txt"
-    path = os.path.join(dir, fileName)
-    file = open(path, "r")
-
-    rawData = json.load(file)
-    candleData = list(rawData['bars'])
     df = dict(Date=[], Open=[], Close=[], High=[], Low=[])
-
-    smas = get_smas(candleData, 9)
-
-    for value in candleData:
-        df['Date'].append(value['t'])
-        df['Open'].append(value['o'])
-        df['Close'].append(value['c'])
-        df['High'].append(value['h'])
-        df['Low'].append(value['l'])
+    for aggregate in aggregateData:
+        df['Date'].append(aggregate.time)
+        df['Open'].append(aggregate.open)
+        df['Close'].append(aggregate.close)
+        df['High'].append(aggregate.high)
+        df['Low'].append(aggregate.low)
 
     fig = go.Figure(data=[
         go.Candlestick(
@@ -30,29 +23,13 @@ def main():
             low=df['Low'],
             close=df['Close'])])
 
+    '''
     fig.add_scatter(
         x=smas['Date'],
         y=smas['Value']
     )
-
+    '''
     fig.write_html('Denali_Results.html', auto_open=True)
-
-
-def get_smas(candleData, emaBacklook):
-
-    smas = dict(Value=[], Date=[])
-    for index, value in enumerate(candleData):
-        if (index < emaBacklook - 1):
-            continue
-
-        sum = 0
-        for x in range(index - (emaBacklook), index):
-            sum += candleData[x]['c']
-
-        smas['Value'].append(sum / emaBacklook)
-        smas['Date'].append(candleData[x]['t'])
-
-    return smas
 
 if __name__ == "__main__":
     main()
---
+++ Denali.Visualizer/MovingAverages.py
@@ -0,0 +1,34 @@
+def get_emas(candleData, emaBacklog):
+    smas = get_smas(candleData, emaBacklog)
+    smoothingConstant = (2 / (emaBacklog + 1))
+
+    emas = dict(Value=[], Date=[])
+    for index, sma in enumerate(smas):
+        if len(emas) == 0:
+            emas['Date'].append(sma['Date'])
+            emas['Value'].append(sma['Value'])
+        else:
+            emas['Date'].append(sma['Date'])
+
+            previousEma = emas['Value'][index - 1]
+            currentEma = (candleData[sma['CandleIndex']] - previousEma) * smoothingConstant + previousEma
+            emas['Value'].append(currentEma)
+
+    return get_smas(candleData, emaBacklog)
+
+def get_smas(candleData, smaBacklog):
+
+    smas = dict(Value=[], Date=[], CandleIndex=[])
+    for index in range(0, len(candleData) - 1):
+        if (index < smaBacklog - 1):
+            continue
+
+        sum = 0
+        for i in range(index - (smaBacklog), index):
+            sum += candleData[i]['c']
+
+        smas['Value'].append(sum / smaBacklog)
+        smas['Date'].append(candleData[index]['t'])
+        smas['CandleIndex'].append(index)
+
+    return smas
