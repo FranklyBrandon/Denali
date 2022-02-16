36843c6d3b6c9a95aa8bd05055fe88077efc65c2
FranklyBrandon <bran.frankenfield@gmail.com>
2022-02-15 22:00:30 -0600

    Add average range TA

---
+++ Denali.V2/Denali.Shared/DecimalExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Denali.Shared
+{
+    public static class DecimalExtensions
+    {
+        public static decimal RoundToMoney(this decimal value)
+        {
+            return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ Denali.V2/Denali.TechnicalAnalysis/AverageRange.cs
@@ -0,0 +1,62 @@
+﻿using Alpaca.Markets;
+using Denali.Shared;
+
+namespace Denali.TechnicalAnalysis
+{
+    /// <summary>
+    /// TA for finding the moving average range of a bar list. 
+    /// Note: This is NOT the 'average true range' (ATR) indicator but rather the literal average range of the bars.
+    /// </summary>
+    public class AverageRange
+    {
+        public List<BarRange> AverageRanges;
+
+        private readonly int _backlog;
+
+        public AverageRange(int backlog)
+        {
+            this._backlog = backlog;
+            this.AverageRanges = new List<BarRange>();
+        }
+
+        public AverageRange(int backlog, IEnumerable<IBar> bars)
+        {
+            this._backlog = backlog;
+            this.AverageRanges = new List<BarRange>();
+        }
+
+        public void Analyze(IEnumerable<IBar> bars)
+        {
+            var length = bars.Count() - 1;
+            if (length < _backlog - 1)
+                return;
+
+            var limit = length - (_backlog - 1);
+
+            var totalRange = 0M;
+            var bodyRange = 0M;
+            for (int i = length; i >= limit; i--)
+            {
+                var bar = bars.ElementAt(i);
+                totalRange += (bar.High - bar.Low);
+                bodyRange += (Math.Abs(bar.Open - bar.Low));
+            }
+
+            AverageRanges.Add(
+                new BarRange((bodyRange / _backlog).RoundToMoney(), (totalRange / _backlog).RoundToMoney())
+            );
+        }
+    }
+
+    public struct BarRange
+    {
+        public decimal BodyRange;
+        public decimal TotalRange;
+
+        public BarRange(decimal bodyRange, decimal totalRange)
+        {
+            this.BodyRange = bodyRange;
+            this.TotalRange = totalRange;
+        }
+    }
+}
