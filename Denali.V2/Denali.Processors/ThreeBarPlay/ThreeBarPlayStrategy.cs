d7c5a878646499c78334fcd99f8ad68c6d6f6d41
FranklyBrandon <bran.frankenfield@gmail.com>
2022-04-10 21:35:12 -0500

    add notes

--- Denali.V2/Denali.Processors/ThreeBarPlay/ThreeBarPlayStrategy.cs
+++ Denali.V2/Denali.Processors/ThreeBarPlay/ThreeBarPlayStrategy.cs
@@ -32,3 +32,13 @@ namespace Denali.Processors.ThreeBarPlay
         }
     }
 }
+
+/*
+ - The size of the candle is determined by comparing a range of previous candles (you can set the amount at your discretion)
+- Search factor: by default 1.3, this means that all bars that have a range greater than the average range of previous candles + 30%, are considered elephant candles (can be configured at your discretion)
+- Possibility to configure the percentage of the body that the elephant candle must have.
+- Possibility of filtering up to 2 means with direction detection and color change (fully configurable)
+- Possibility of filtering by mobile averages
+
+https://www.tradingview.com/script/yV11ifDS-Elephant-Bar-by-Oliver-Velez/
+ */
