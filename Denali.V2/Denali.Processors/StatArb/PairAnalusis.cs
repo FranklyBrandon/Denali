98eff2fef2be3c5f7c03c29ec9d06e328fc251e0
FranklyBrandon <bran.frankenfield@gmail.com>
2022-07-28 20:51:40 -0500

    add stat arb in progress

---
+++ Denali.V2/Denali.Processors/StatArb/PairAnalusis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denali.Processors.StatArb
+{
+    internal class PairAnalusis
+    {
+        //https://www.reddit.com/r/algotrading/comments/obbb5d/kalman_filter_stat_arb/
+    }
+}
