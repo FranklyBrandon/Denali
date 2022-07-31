fca7efc77928df7c4be950152e9d735a941b4b94
FranklyBrandon <bran.frankenfield@gmail.com>
2022-07-31 00:15:38 -0500

    add pair anyalisis in progress

---
+++ Denali.V2/Denali.Processors/StatArb/PairAnalysis.cs
@@ -0,0 +1,52 @@
+﻿using Alpaca.Markets;
+using AutoMapper;
+using Denali.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denali.Processors.StatArb
+{
+    public class PairAnalysis : StrategyProcessorBase
+    {
+        public PairAnalysis(AlpacaService alpacaService, IMapper mapper) : base(alpacaService, mapper)
+        {
+
+        }
+
+        public async void Process(string tickerA, string tickerB, DateTime startDate, DateTime endDate, BarTimeFrame barTimeFrame, int numberOfBacklogDays, CancellationToken stoppingToken)
+        {
+            _alpacaService.InitializeTradingclient();
+            _alpacaService.InitializeDataClient();
+
+            var marketDays = await GetOpenMarketDays(startDate, endDate);
+            if (marketDays != null && marketDays.Any())
+            {
+                var tickerAbars = await _alpacaService.AlpacaDataClient.ListHistoricalBarsAsync(
+                    new HistoricalBarsRequest(tickerA, marketDays.First().GetTradingOpenTimeUtc(), marketDays.Last().GetTradingCloseTimeUtc(), barTimeFrame));
+
+                var tickerBbars = await _alpacaService.AlpacaDataClient.ListHistoricalBarsAsync(
+                    new HistoricalBarsRequest(tickerB, marketDays.First().GetTradingOpenTimeUtc(), marketDays.Last().GetTradingCloseTimeUtc(), barTimeFrame));
+
+                if (tickerAbars.Items.Count != tickerBbars.Items.Count)
+                    throw new ArgumentOutOfRangeException("Historic pair timeframe data frames do not match");
+
+
+            }
+
+
+
+
+        }
+
+        private decimal PercentageDifference(decimal originalValue, decimal newValue)
+        {
+            return Math.Abs(originalValue - newValue) / originalValue;
+        }
+
+        //https://www.reddit.com/r/algotrading/comments/obbb5d/kalman_filter_stat_arb/
+        //https://algotrading101.com/learn/quantitative-trader-guide/#how-can-I-create-a-pairs-trading-strategy
+    }
+}
---
+++ Denali.V2/Denali.Processors/StrategyProcessorBase.cs
@@ -0,0 +1,35 @@
+﻿using Alpaca.Markets;
+using AutoMapper;
+using Denali.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denali.Processors
+{
+    public abstract class StrategyProcessorBase
+    {
+        protected readonly AlpacaService _alpacaService;
+        protected readonly IMapper _mapper;
+
+        public StrategyProcessorBase(AlpacaService alpacaService, IMapper mapper)
+        {
+            _alpacaService = alpacaService;
+            _mapper = mapper;
+        }
+
+        protected async Task<IEnumerable<IIntervalCalendar>> GetOpenBacklogDays(int pastDays, DateTime day)
+        {
+            var calenders = await _alpacaService.AlpacaTradingClient.ListIntervalCalendarAsync(new CalendarRequest().WithInterval(new Interval<DateTime>(day.AddDays(-pastDays), day)));
+            return calenders.OrderByDescending(x => x.GetTradingDate());
+        }
+
+        protected async Task<IEnumerable<IIntervalCalendar>> GetOpenMarketDays(DateTime from, DateTime into)
+        {
+            var calenders = await _alpacaService.AlpacaTradingClient.ListIntervalCalendarAsync(new CalendarRequest().WithInterval(new Interval<DateTime>(from, into)));
+            return calenders.OrderByDescending(x => x.GetTradingDate());
+        }
+    }
+}
