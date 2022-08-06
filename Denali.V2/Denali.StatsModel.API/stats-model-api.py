1498016ffe5005295f37a97f034cfb3ad65ee282
FranklyBrandon <bran.frankenfield@gmail.com>
2022-08-06 00:26:58 -0500

    add stats model api

--- Denali.V2/Denali.StatsModel.API/stats-model-api.py
+++ Denali.V2/Denali.StatsModel.API/stats-model-api.py
@@ -1,11 +1,18 @@
+import json
 from flask import Flask, jsonify, request
+import statsmodels.tsa.stattools as sm
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello_get():
+@app.route('/ols', methods=['GET'])
+def ols_get():
     data = request.get_json(force=True)
-    return jsonify({"msg": "hi!"})
+    x = data['x']
+    y = data['y']
+    constY = sm.add_constant(y)
+    result = sm.OLS(x, constY).fit()
+    return jsonify(result)
+    #beta = result.params['close']
 
 if __name__ == '__main__':
     app.run()
