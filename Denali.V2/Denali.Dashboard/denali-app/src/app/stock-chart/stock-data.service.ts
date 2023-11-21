c295ddced2992c04a6ca4c35baa39c9320316fa9
FranklyBrandon <bran.frankenfield@gmail.com>
2023-11-20 19:57:56 -0500

    add environment configuration

--- Denali.V2/Denali.Dashboard/denali-app/src/app/stock-chart/stock-data.service.ts
+++ Denali.V2/Denali.Dashboard/denali-app/src/app/stock-chart/stock-data.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockDataService {
+  private baseURL: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.baseURL = `${environment.apiURL}/${environment.stockDataPath}`
+   }
 
   public getStockAggregateData(ticker: string) : Observable<any> {
-    return this.http.get(`https://localhost:7016/stocks/${ticker}/aggregate?startDateTime=2023-04-06T14%3A30%3A00Z&endDateTime=2023-04-06T21%3A00%3A00Z&timeFrame=1`)
+    return this.http.get(`${this.baseURL}/${ticker}/aggregate?startDateTime=2023-04-06T14%3A30%3A00Z&endDateTime=2023-04-06T21%3A00%3A00Z&timeFrame=1`)
   }
 }
---
+++ Denali.V2/Denali.Dashboard/denali-app/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiURL: 'https://localhost:7016',
+    stockDataPath: 'stocks'
+};
